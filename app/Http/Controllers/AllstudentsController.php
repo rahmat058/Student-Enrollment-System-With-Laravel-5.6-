3e1c40a0919129fae96c51e56ecd0b4aea3e41df
Kazi Rahamatullah <kazirahmat58@gmail.com>
2018-03-21 03:13:30 +0600

    All Students show is done

--- app/Http/Controllers/AllstudentsController.php
+++ app/Http/Controllers/AllstudentsController.php
@@ -3,10 +3,18 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use DB;
 
 class AllstudentsController extends Controller
 {
   public function allStudents() {
-    return view('admin.allstudent');
+    $allstudent_info = DB::table('student_tbl')
+                      -> get();
+
+    $manage_student = view('admin.allstudent')
+                      -> with ('allstudent_info', $allstudent_info);
+
+    return view('layout')
+                -> with('allstudent', $manage_student);
   }
 }
--- resources/views/admin/allstudent.blade.php
+++ resources/views/admin/allstudent.blade.php
@@ -3,7 +3,7 @@
 
   <div class="card">
     <div class="card-body">
-      <h2 class="card-title">Data table</h2>
+      <h2 class="card-title">All Student</h2>
       <div class="row">
         <div class="col-12">
           <table id="order-listing" class="table table-striped" style="width:100%;">
@@ -19,19 +19,35 @@
               </tr>
             </thead>
             <tbody>
+
+              @foreach($allstudent_info as $all_student)
               <tr>
-                  <td>1</td>
-                  <td>27/08/12</td>
-                  <td>Edinburgh</td>
-                  <td>New York</td>
-                  <td>$1500</td>
-                  <td>$3200</td>
+                  <td>{{$all_student->student_roll}}</td>
+                  <td>{{$all_student->student_name}}</td>
+                  <td>{{$all_student->student_phone}}</td>
+                  <td><img src="{{URL::to($all_student->student_image)}}" height="80" width="100" style="border-radius: 50%;"></td>
+                  <td>{{$all_student->student_address}}</td>
+                  <td>
+                      @if ($all_student->student_department == 1)
+                          <span class="label label-success">{{'CSE'}}</span>
+                      @elseif ($all_student->student_department == 2)
+                         <span class="label label-primary">{{'ECE'}}</span>
+                      @elseif ($all_student->student_department == 3)
+                         <span class="label label-warning">{{'BBA'}}</span>
+                      @elseif ($all_student->student_department == 4)
+                         <span class="label label-info">{{'EEE'}}</span>
+                      @elseif ($all_student->student_department == 5)
+                         <span class="label label-important">{{'MBA'}}</span>
+                      @endif
+                  </td>
                   <td>
                     <button class="btn btn-outline-primary">View</button>
                     <button class="btn btn-outline-warning">Edit</button>
                     <button class="btn btn-outline-danger">Delete</button>
                   </td>
               </tr>
+              @endforeach
+
             </tbody>
           </table>
         </div>
